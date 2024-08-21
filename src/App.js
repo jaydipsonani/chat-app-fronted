ea2b9e43df92113fcdbbcf90b477520277f3653c
Jaydip Sonani <jaydip.sonani@cloudusinfotech.com>
2024-08-21 13:45:55 +0530

    chat-app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import io from "socket.io-client";
 import Chat from "./Chat";
 
-const socket = io.connect("https://chat-app-backend-gqqg.onrender.com");
+const socket = io.connect("https://chat-app-backend-4ihp.onrender.com");
 
 function App() {
   const [username, setUsername] = useState("");
