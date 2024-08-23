03e29fc9155138d6063b47c2aa5d1a3a6dd4f907
jaydipsonani <132323340+jaydipsonani@users.noreply.github.com>
2024-08-23 12:40:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import io from "socket.io-client";
 import Chat from "./Chat";
 
-const socket = io.connect("https://chat-app-backend-bay-seven.vercel.app");
+const socket = io.connect("https://chat-app-backend-lyart.vercel.app");
 
 function App() {
   const [username, setUsername] = useState("");
